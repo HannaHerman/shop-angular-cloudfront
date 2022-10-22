e1da4746ba760d5cce1d5bd8705f56e3e0a78ba7
HannaHerman <mrs.hanna.herman@gmail.com>
2022-10-22 23:00:17 +0200

    fix: fix url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://pmakio4xqd.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://kw5so0b5nb.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
